62bf580d33340625a1c2dfc64fefc2e7c9132669
Jinwglasses1 <202503460931@alunos.estacio.br>
2025-03-24 20:11:37 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,18 +6,20 @@ int main() {
     char estado, estado2; // uma letra de A a H
     char codigo[10], codigo2[10]; // letra do estado seguido de uma número
     char nome[10], nome2[10]; // nome da cidade
-    int populacao, populacao2;
+    int  populacao, populacao2;
     float area, area2; // área da cidade em km²
     float pib, pib2;
     int pontos_turisticos, pontos_turisticos2; // número de pontos turísticos
     float densidade1, densidade2;
     float pibper1, pibper2;
+    float superpoder1, superpoder2;
+    int resultadopopulacao, resultadoarea, resultadopib, resultadopontos, resultadopibper, resultadodensidade, resultadosuper;
 
 
     printf("Carta 01 \n");
 
     printf("Estado:");
-    scanf(" %c", &estado);
+    scanf(" %c", &estado); // apenas uma letra
 
     printf("Código:");
     scanf(" %s", codigo);
@@ -28,7 +30,7 @@ int main() {
     printf("População:");
     scanf("%d", &populacao);
 
-    printf("Área:");
+    printf("Área(km²):");
     scanf("%f", &area);
 
     printf("PIB:");
@@ -44,25 +46,29 @@ int main() {
     
     printf( "PIB per capita: %.2f \n", pibper1);
 
+    superpoder1 = (float) populacao + area + pib + pontos_turisticos + pibper1 + 1/densidade1;
+    
+    printf("O poder da carta 01 é: %.0f\n", superpoder1);
+
     printf("Carta 02 \n");
 
-    printf("Estado:");
-    scanf(" %c",&estado2);
+    printf("Estado: ");
+    scanf(" %c", &estado2); // apenas uma letra
 
     printf("Código: ");
     scanf(" %s", codigo2);
 
     printf("Nome da cidade: ");
-    scanf(" %s", nome2);
+    scanf("%s", nome2);
 
     printf("População: ");
-    scanf(" %d", &populacao2);
+    scanf("%d", &populacao2);
 
-    printf("Área: ");
-    scanf(" %f", &area2);
+    printf("Área(km²): ");
+    scanf("%f", &area2);
 
     printf("PIB:");
-    scanf(" %f", &pib2);
+    scanf("%f", &pib2);
 
     printf("Número de pontos turísticos:");
     scanf(" %d", &pontos_turisticos2);
@@ -74,10 +80,30 @@ int main() {
     
     printf( "PIB per capita: %.2f \n", pibper2);
 
+    superpoder2 = (float) populacao2 + area2 + pib2 + pontos_turisticos2 + pibper2 + 1/densidade2;
 
+    printf("O poder da carta 02 é: %.0f\n", superpoder2);
 
+    resultadopopulacao = populacao > populacao2;
+    resultadoarea = area > area2;
+    resultadopib = pib > pib2;
+    resultadopontos = pontos_turisticos > pontos_turisticos2;
+    resultadopibper = pibper1 > pibper2;
+    resultadodensidade = densidade1 < densidade2;
+    resultadosuper = superpoder1 > superpoder2;
 
+    printf("***Batalha***\n");
 
+    printf("População: %d\n", resultadopopulacao);
+    printf("Área: %d\n", resultadoarea);
+    printf("Pib: %d\n", resultadopib);
+    printf("Pontos turísticos: %d\n", resultadopontos);
+    printf("Pib per capita: %d\n", resultadopibper);
+    printf("Densidade: %d\n", resultadodensidade); // o menor valor vence
+    printf("Poder: %d\n", resultadosuper);
+
+    // Resultado = 1, vitória da carta 01
+    // Resultado = 0, vitória da carta 02
 
      return 0;
 }
